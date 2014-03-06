eeba9e482139d00dd97bee951b6593ac045b68a9
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-03-06 18:35:30 +0100

    Update CMake subtree to get new doxygit layout

--- Lunchbox-1.9/search/classes_63.js
+++ Lunchbox-1.9/search/classes_63.js
@@ -2,7 +2,5 @@ var searchData=
 [
   ['clock',['Clock',['../classlunchbox_1_1_clock.html',1,'lunchbox']]],
   ['compressor',['Compressor',['../classlunchbox_1_1_compressor.html',1,'lunchbox']]],
-  ['compressorchunk',['CompressorChunk',['../structlunchbox_1_1_compressor_chunk.html',1,'lunchbox']]],
-  ['compressorresult',['CompressorResult',['../structlunchbox_1_1_compressor_result.html',1,'lunchbox']]],
   ['condition',['Condition',['../classlunchbox_1_1_condition.html',1,'lunchbox']]]
 ];
